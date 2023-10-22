17097605f7ec636cc90c69bc5337a219cf637b2e
Fernando <evilmage39@gmail.com>
2023-10-21 20:54:40 -0500

    tarea-2-service

--- plugins/services.js
+++ plugins/services.js
@@ -3,6 +3,7 @@ import { UsuarioService } from '@/services/usuario.service'
 import { ProyectoService } from '@/services/proyecto.service'
 import { TipoService } from '@/services/tipo.service'
 import { EquipoService } from '@/services/equipo.service'
+import { TareaService } from '@/services/tarea.service'
 
 export default function (ctx,inject){
     if(!ctx.$axios){
@@ -14,4 +15,5 @@ export default function (ctx,inject){
     inject('proyecto',new ProyectoService(ctx))
     inject('tipo',new TipoService(ctx))
     inject('equipo',new EquipoService(ctx))
+    inject('tarea',new TareaService(ctx))
 }
