cbc730f574124917baacc1f685e3785ea63d5e0e
Erick <erickhbss2002@gmail.com>
2023-10-21 20:46:37 -0500

    equipo-service

--- plugins/services.js
+++ plugins/services.js
@@ -2,6 +2,7 @@ import { ClienteService } from '@/services/cliente.service'
 import { UsuarioService } from '@/services/usuario.service'
 import { ProyectoService } from '@/services/proyecto.service'
 import { TipoService } from '@/services/tipo.service'
+import { EquipoService } from '@/services/equipo.service'
 
 export default function (ctx,inject){
     if(!ctx.$axios){
@@ -12,4 +13,5 @@ export default function (ctx,inject){
     inject('usuario',new UsuarioService(ctx))
     inject('proyecto',new ProyectoService(ctx))
     inject('tipo',new TipoService(ctx))
+    inject('equipo',new EquipoService(ctx))
 }
