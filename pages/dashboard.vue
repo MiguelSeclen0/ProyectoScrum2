875bf75febb41843203d758b061ee174696cf31d
Your Name <axelculqui@gmail.com>
2023-10-23 22:02:21 -0500

    dashito cambios 2

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,7 +1,7 @@
 <template>
     <v-row>
         <v-col>
-            <v-row style="margin-top: 2%; padding-left: 2%;" class="justify-end">
+            <!-- <v-row style="margin-top: 2%; padding-left: 2%;" class="justify-end">
                 <UserMenu :name="usersName()">
                     <v-list subheader>
                         <v-subheader>Opciones</v-subheader>
@@ -13,13 +13,13 @@
                         </v-list-item>
                     </v-list>
                 </UserMenu>
-            </v-row>
-            <v-row>
-                <v-col md="4">
-                    <v-card>
+            </v-row> -->
+            <v-row justify-end>
+                <v-col md="3">
+                    <v-card >
                         <v-list-item three-line>
-                            <v-list-item-avatar tile size="50" color="grey">
-                                <v-icon>mdi-format-list-bulleted</v-icon>
+                            <v-list-item-avatar tile size="50" style="background: #FFF176 !important;" class="pa-4 text-center secondary text-no-wrap rounded-lg">
+                                <v-icon color="black">mdi-format-list-bulleted</v-icon>
                             </v-list-item-avatar>
                             <v-list-item-content>
                                 <div class="text-overline mb-4">
@@ -32,7 +32,7 @@
                         </v-list-item>
                     </v-card>
                 </v-col>
-                <v-col md="4">
+                <v-col md="3">
                     <v-card>
                         <v-list-item three-line>
                             <v-list-item-avatar tile size="50" style="background: #FFB74D !important;" class="pa-4 text-center secondary text-no-wrap rounded-lg">
@@ -49,11 +49,12 @@
                         </v-list-item>
                     </v-card>
                 </v-col>
-                <v-col md="4">
+                <v-col md="3">
                     <v-card>
                         <v-list-item three-line>
-                            <v-list-item-avatar tile size="50"><svg-icon type="mdi"
-                                    :path="path"></svg-icon></v-list-item-avatar>
+                            <v-list-item-avatar tile size="50" style="background: #F8BBD0 !important;" class="pa-4 text-center secondary text-no-wrap rounded-lg">
+                                <v-icon color="black">mdi-poll</v-icon>
+                                </v-list-item-avatar>
                             <v-list-item-content>
                                 <div class="text-overline mb-4">
                                     ASIGNACIONES EN PROCESO
@@ -65,13 +66,26 @@
                         </v-list-item>
                     </v-card>
                 </v-col>
+                <v-col md="3">
+                    <UserMenu :name="usersName()">
+                    <v-list subheader>
+                        <v-subheader>Opciones</v-subheader>
+                        <v-list-item @click.stop="onLogout">
+                            <v-list-item-icon>
+                                <v-icon>mdi-logout-variant</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-title>Cerrar Sesion</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </UserMenu>
+                </v-col>
             </v-row>
         </v-col>
     </v-row>
 </template>
   
 <script>
-export default {
+export default {    
     name: 'dashboard',
     layout: 'default',
     data: () => ({
